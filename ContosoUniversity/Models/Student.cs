7c747a217920a98bff95c827a30b140e402c0d0f
Brad <bradkellogg7@gmail.com>
2019-03-30 23:54:54 -0600

    Finished tutorial

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student : Person
+    {
+        [DataType(DataType.Date)]
+        [Display(Name = "Enrollment Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
